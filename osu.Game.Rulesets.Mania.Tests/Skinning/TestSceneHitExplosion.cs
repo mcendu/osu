da1f61b8f7abcff99ad0163dfeaccb467a029c89
mcendu <nathandu@outlook.com>
2020-04-02 13:46:43 +0800

    add tests

--- osu.Game.Rulesets.Mania.Tests/Skinning/TestSceneHitExplosion.cs
+++ osu.Game.Rulesets.Mania.Tests/Skinning/TestSceneHitExplosion.cs
@@ -4,21 +4,24 @@
 using System;
 using System.Collections.Generic;
 using NUnit.Framework;
+using osu.Framework.Allocation;
+using osu.Framework.Extensions.IEnumerableExtensions;
 using osu.Framework.Graphics;
 using osu.Game.Rulesets.Mania.Objects.Drawables;
 using osu.Game.Rulesets.Mania.Objects.Drawables.Pieces;
 using osu.Game.Rulesets.Mania.UI;
 using osu.Game.Rulesets.UI.Scrolling;
+using osu.Game.Skinning;
 using osu.Game.Tests.Visual;
 using osuTK;
 using osuTK.Graphics;
 
-namespace osu.Game.Rulesets.Mania.Tests
+namespace osu.Game.Rulesets.Mania.Tests.Skinning
 {
     [TestFixture]
-    public class TestSceneHitExplosion : OsuTestScene
+    public class TestSceneHitExplosion : SkinnableTestScene
     {
-        private ScrollingTestContainer scrolling;
+        private int runCount;
 
         public override IReadOnlyList<Type> RequiredTypes => new[]
         {
@@ -26,36 +29,46 @@ namespace osu.Game.Rulesets.Mania.Tests
             typeof(DrawableManiaHitObject),
         };
 
-        protected override void LoadComplete()
+        [BackgroundDependencyLoader]
+        private void load()
         {
-            base.LoadComplete();
-
-            Child = scrolling = new ScrollingTestContainer(ScrollingDirection.Down)
+            SetContents(() => new ScrollingTestContainer(ScrollingDirection.Down)
             {
                 Anchor = Anchor.Centre,
                 Origin = Anchor.Centre,
                 RelativePositionAxes = Axes.Y,
                 Y = -0.25f,
                 Size = new Vector2(Column.COLUMN_WIDTH, DefaultNotePiece.NOTE_HEIGHT),
-            };
+            });
+        }
 
-            int runcount = 0;
+        [SetUp]
+        public void SetUp()
+        {
+            runCount = 0;
+        }
 
+        [Test]
+        public void Test()
+        {
             AddRepeatStep("explode", () =>
             {
-                runcount++;
+                runCount++;
 
-                if (runcount % 15 > 12)
+                if (runCount % 15 > 12)
                     return;
 
-                scrolling.AddRange(new Drawable[]
+                CreatedDrawables.ForEach(d => (d as ScrollingTestContainer)?.AddRange(new Drawable[]
                 {
-                    new DefaultHitExplosion((runcount / 15) % 2 == 0 ? new Color4(94, 0, 57, 255) : new Color4(6, 84, 0, 255), runcount % 6 != 0)
+                    new SkinnableDrawable(new ManiaSkinComponent(ManiaSkinComponents.HitExplosion),
+                        _ => new DefaultHitExplosion((runCount / 15) % 2 == 0
+                            ? new Color4(94, 0, 57, 255)
+                            : new Color4(6, 84, 0, 255), runCount % 6 != 0))
                     {
                         Anchor = Anchor.Centre,
                         Origin = Anchor.Centre,
                     }
-                });
+                }));
             }, 100);
         }
     }
